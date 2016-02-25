e3455ca78545b6ff05681b3b2de84e64fbe8df35
Bertrand Gondouin <gondouin@gmail.com>
2016-02-25 13:15:45 +0100

    added Ionic project

---
+++ README.md
@@ -0,0 +1,22 @@
+# HM11 PIO
+This project is a [Bluetooth V4.0 HM-11 BLE module](http://www.jnhuamao.cn/bluetooth.asp?ID=1) control panel built with [Ionic Framework](http://ionicframework.com/) using the [Cordova BLE plugin](https://github.com/evothings/cordova-ble) from [Evothings](https://evothings.com/). It was mainly a toy project to start a small and cheap LED and BLE notifier.
+
+Built on top of Ionic, this is a hybrid HTML5 app meant to be run via Ionic or cordova, and not as a web app.I've tested it on Android 4.4 but should work on most modern devices and iDevices.
+
+## HM-11 BLE module
+![Control Panel](www/img/hm11.png?raw=true "")
+
+## HM-11 BLE module I/O lines
+HM-11 has 2 Programmable I/O lines:
+
+* PIO3: low/high
+* PIO2: low/high + PWM (100ms -> 800ms)
+(source: [Datasheet](https://www.seeedstudio.com/wiki/images/c/cd/Bluetooth4_en.pdf))
+
+## Run it
+1. Make sure to have [Ionic installed](http://ionicframework.com/docs/guide/installation.html).
+2. `git clone http://www.github.com/bgond/hm11-pio && cd hm11-pio`
+3. Run with Ionic: `ionic run android`
+
+## BLE LED notifier
+![Notifier](www/img/littleblock.jpg?raw=true "")
---
+++ www/js/app.js
@@ -0,0 +1,107 @@
+angular.module('littleBlock', ['ionic'])
+    .run(function($ionicPlatform, $rootScope) {
+        $ionicPlatform.ready(function() {
+
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+
+                if (evothings.ble) {
+                    console.log('evothings.ble loaded');
+                    $rootScope.$broadcast('ble-started');
+                } else {
+                    console.log('no evothings.ble');
+                }
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+.controller('HM11Ctrl', function($scope) {
+
+    $scope.$on('ble-started', function(event, args) {
+        $scope.connect();
+    });
+
+    // Connected device
+    var HM11 = null;
+
+    // Device name
+    var device_name = 'ioblocks';
+
+    //characteristic UUID we want to send commands to
+    var UUID = '0000ffe1-0000-1000-8000-00805f9b34fb';
+
+    //Digital IO values
+    $scope.PIO3 = {
+        checked: false
+    };
+    $scope.PIO2 = {
+        checked: false,
+        pwm: 2
+    };
+
+    $scope.connect = function() {
+        evothings.arduinoble.close();
+        evothings.arduinoble.connect(
+            device_name,
+            function(device) {
+                HM11 = device;
+                console.log('Connected!');
+            },
+            function(errorCode) {
+                console.log('Connect error: ' + errorCode + '.');
+            });
+    };
+
+    $scope.PIO3Change = function() {
+        if ($scope.PIO3.checked) {
+            // Turn on LED AT Commands: Setup PIO3 output high.
+            str = 'AT+PIO31';
+        } else {
+            // Turn off LED AT Commands: Setup PIO3 output low.
+            str = 'AT+PIO30';
+        }
+        uint = new Uint8Array(str.length);
+        for (var i = 0, j = str.length; i < j; ++i) {
+            uint[i] = str.charCodeAt(i);
+        }
+        HM11.writeDataArray(UUID, uint);
+    };
+
+    $scope.PIO2Change = function() {
+        if ($scope.PIO2.checked) {
+            // Turn on LED AT Commands: Setup PIO2 output high.
+            str = 'AT+PIO21';
+        } else {
+            // Turn off LED AT Commands: Setup PIO2 output low.
+            str = 'AT+PIO20';
+        }
+        uint = new Uint8Array(str.length);
+        for (var i = 0, j = str.length; i < j; ++i) {
+            uint[i] = str.charCodeAt(i);
+        }
+        HM11.writeDataArray(UUID, uint);
+    };
+
+    $scope.PIO2PWM = function() {
+        // V525 + firmware (tested on V537) added PIO2 PWM function, Para2 value is 0~9
+        // 2: output 100ms PWM
+        // 3: output 200ms PWM
+        // ......
+        // 9: output 800ms PWM
+        str = 'AT+PIO2' + $scope.PIO2.pwm;
+        uint = new Uint8Array(str.length);
+        for (var i = 0, j = str.length; i < j; ++i) {
+            uint[i] = str.charCodeAt(i);
+        }
+        HM11.writeDataArray(UUID, uint);
+    };
+});
